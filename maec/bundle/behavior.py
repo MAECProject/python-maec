68044b5d802bff7478707e9cbc9a3ed250574436
Andrew Sillers <apsillers@gmail.REMOVETHISSECTION.com>
2013-03-22 10:35:42 -0400

    Expanded behavior.py

--- maec/bundle/behavior.py
+++ maec/bundle/behavior.py
@@ -11,28 +11,47 @@ import cybox.bindings.commons as cybox_common
 import datetime
        
 class Behavior(object):
-    def init(self, generator):
-        self.generator = generator;
+    def init(self, generator, id, description, ordinal_position):
+        if id is not None:
+            self.id = id
+        elif generator is not None:
+            self.generator = generator;
+            self.id = self.generator.generate_bundle_id()
+        else:
+            raise Exception("Must specify id or generator for Behavior constructor")
         
-        self.behavior = bundle_binding.BehaviorType(id=self.generator.generate_bundle_id())
+        self.behavior = bundle_binding.BehaviorType(self.id)
         self.discoveryMethod = cybox_common.MeasureSourceType()
         self.actionComposition = bundle_binding.BehavioralActionsType()
         self.associatedCode = bundle_binding.AssociatedCodeType()
         self.relationships = bundle_binding.BehaviorRelationshipListType()
         
-    def set_purpose(self, purpose):
-        pass
+        self.purpose = bundle_binding.BehaviorPurposeType()
+        
+        self.description = description
+        self.ordinal_position = ordinal_position
     
+    def set_known_vulnerability(self, cve_id, description):
+        vuln = bundle_binding.VulnerabilityExploitType()
+        cve = bundle_binding.CVEVulnerabilityType()
+        cve.set_cve_id(cve_id)
+        cve.set_Description(description)
+        vuln.set_CVE(cve)
+        vuln.set_known_vulnerability(True)
+        self.purpose.set_Vulnerability_Exploit(vuln)
+        
     def set_description(self, description):
         self.behavior.set_Description(description)
         
-    def set_action_composition(self, action_composition):
-        pass
-        
-    def add_related_behavior(self, related_behavior_ref):
-        relationship = bundle_binding.BehaviorRelationshipType()
-        relationship.add_Behavior_Reference(related_behavior_ref)
-        self.relationships.add_Relationship(relationship)   
+    def add_action(self, action):
+        action_ref = bundle_binding.BehavioralActionReferenceType(action_id=action.get_idref(), behavioral_ordering=action.get_behavioral_ordering())
+        self.actionComposition.add_Action_Reference(action_ref)
+
+    def add_related_behavior(self, type, behavior):
+        relationship = bundle_binding.BehaviorRelationshipType(type_=type)
+        behavior_ref = bundle_binding.BehaviorReferenceType(behavior_idref = behavior.get_idref())
+        relationship.add_Behavior_Reference(behavior_ref)
+        self.relationships.add_Relationship(relationship)
         
     def set_oridinal_position(self, position):
         self.behavior.set_ordinal_position(position)
@@ -50,4 +69,5 @@ class Behavior(object):
         if self.actionComposition.hasContent_(): self.behavior.set_Action_Composition(self.actionComposition)
         if self.associatedCode.hasContent_(): self.behavior.set_Associated_Code(self.associatedCode)
         if self.relationships.hasContent_(): self.behavior.set_Relationships(self.relationships)
-    
+        if self.purpose.hasContent_(): self.behavior.set_Purpose(self.purpose)
+        
