11c8bec6a58c25dca4715c15f7509c2e74d366dc
Ivan Kirillov <ikirillov@mitre.org>
2013-05-13 10:40:11 -0400

    Object reference initial commit

---
+++ maec/bundle/object_reference.py
@@ -0,0 +1,42 @@
+#MAEC Object Reference Class
+
+#Copyright (c) 2013, The MITRE Corporation
+#All rights reserved.
+
+#Compatible with MAEC v4.0
+#Last updated 05/10/2013
+
+import maec
+import maec.bindings.maec_bundle as bundle_binding       
+
+class ObjectReference(maec.Entity):
+    def init(self, object_idref = None):
+        super(ObjectReference, self).__init__()
+        self.object_idref = object_idref
+
+    def to_obj(self):
+        object_reference_obj = bundle_binding.ObjectReferenceType()
+        if self.object_idref is not None : object_reference_obj.set_object_idref(self.object_idref)
+        return object_reference_obj
+
+    def to_dict(self):
+        object_reference_dict = {}
+        if self.object_idref is not None : object_reference_dict['object_idref'] = self.object_idref
+        return object_reference_dict
+
+    @staticmethod
+    def from_dict(object_reference_dict):
+        if not object_reference_dict:
+            return None
+        object_reference_ = ObjectReference()
+        object_reference_.object_idref = object_reference_dict.get('object_idref')
+        return object_reference_
+
+    @staticmethod
+    def from_obj(object_reference_obj):
+        if not object_reference_obj:
+            return None
+        object_reference_ = ObjectReference()
+        object_reference_.object_idref = object_reference_obj.get_object_idref()
+        return object_reference_
+        
