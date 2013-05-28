4dee2dc40cbdfdd0ae9fc5a204f7e293e2b562d2
Ivan Kirillov <ikirillov@mitre.org>
2013-05-28 14:28:24 -0400

    Updated namespaceparser logic for elegance; it now accepts a single parameter some method names were changed for simplicity and brevity

--- maec/utils/nsparser.py
+++ maec/utils/nsparser.py
@@ -4,27 +4,27 @@
 #All rights reserved.
 
 #Compatible with MAEC v4.0
-#Last updated 5/16/2013
+#Last updated 5/28/2013
 
+import maec.bindings.maec_bundle as bundle_binding
+import maec.bindings.maec_package as package_binding
 from cybox.utils import NamespaceParser, OBJECT_TYPES_DICT
 
 import itertools
 
 class MAECNamespaceParser(NamespaceParser):
     superclass = NamespaceParser
-    def __init__(self, bundle = None, package = None):
+    def __init__(self, maec_entity):
         super(MAECNamespaceParser, self).__init__([])
-        self.bundle = bundle
-        self.package = package
-        self.process_namespaces()
-
-    def process_namespaces(self):
-        if self.package is not None: self.process_package_namespace(self.package)
-        elif self.bundle is not None: self.process_bundle_namespace(self.bundle)
+        self.maec_entity = maec_entity
+        if isinstance(self.maec_entity, package_binding.PackageType):
+            self.process_package_namespace(self.maec_entity)
+        elif isinstance(self.maec_entity, bundle_binding.BundleType):
+            self.process_bundle_namespace(self.maec_entity)
 
     def process_package_namespace(self, package):
-        if self.package.get_Malware_Subjects() is not None:
-            for malware_subject in self.package.get_Malware_Subjects().get_Malware_Subject():
+        if package.get_Malware_Subjects() is not None:
+            for malware_subject in package.get_Malware_Subjects().get_Malware_Subject():
                 if malware_subject.get_Malware_Instance_Object_Attributes() is not None:
                     self.get_namespace_from_object(malware_subject.get_Malware_Instance_Object_Attributes())
                 if malware_subject.get_Findings_Bundles() is not None:
@@ -33,7 +33,7 @@ class MAECNamespaceParser(NamespaceParser):
 
     def process_bundle_namespace(self, bundle):
         if bundle.get_Malware_Instance_Object_Attributes() is not None:
-            self.get_namespace_from_object(self.bundle.get_Malware_Instance_Object_Attributes())
+            self.get_namespace_from_object(bundle.get_Malware_Instance_Object_Attributes())
         if bundle.get_Process_Tree() is not None:
             self.add_object_namespace('ProcessObjectType')
         if bundle.get_Behaviors() is not None:
@@ -73,21 +73,21 @@ class MAECNamespaceParser(NamespaceParser):
             for associated_object in action.get_Associated_Objects().get_Associated_Object():
                 self.get_namespace_from_object(associated_object)
    
-    def build_maec_namespaces_schemalocations_str(self):
-        '''Build the namespace/schemalocation declaration string'''
+    def get_namespace_schemalocation_str(self):
+        '''Get the namespace/schemalocation declaration string'''
         
         output_string = '\n '
         schemalocs = []
         #Add the XSI, MAEC, and CybOX Core/Common namespaces and schemalocation
         output_string += 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \n '
-        if self.package is not None:
+        if isinstance(self.maec_entity, package_binding.PackageType):
             output_string += 'xmlns:maecPackage="http://maec.mitre.org/XMLSchema/maec-package-2" \n '
             output_string += 'xmlns:maecBundle="http://maec.mitre.org/XMLSchema/maec-bundle-4" \n '
             output_string += 'xmlns:maecVocabs="http://maec.mitre.org/default_vocabularies-1" \n '
             output_string += 'xmlns:mmdef="http://xml/metadataSharing.xsd" \n '
             schemalocs.append('http://maec.mitre.org/default_vocabularies-1 http://maec.mitre.org/language/version4.0/maec_default_vocabularies.xsd')
             schemalocs.append(' http://maec.mitre.org/XMLSchema/maec-package-2 http://maec.mitre.org/language/version4.0/maec_package_schema.xsd')
-        elif self.bundle is not None:
+        elif isinstance(self.maec_entity, bundle_binding.BundleType):
             output_string += 'xmlns:maecVocabs="http://maec.mitre.org/default_vocabularies-1" \n '
             output_string += 'xmlns:maecBundle="http://maec.mitre.org/XMLSchema/maec-bundle-4" \n '
             output_string += 'xmlns:mmdef="http://xml/metadataSharing.xsd" \n '
