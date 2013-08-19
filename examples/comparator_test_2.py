134e446fe046cbd9b3c2c94786c480f25bd55fb4
Ivan Kirillov <ikirillov@mitre.org>
2013-08-19 16:17:26 -0400

    Updated for match_on testing

--- examples/comparator_test_2.py
+++ examples/comparator_test_2.py
@@ -1,11 +1,16 @@
 import pprint
 import maec.bindings.maec_bundle as maec_bundle_binding
 from maec.bundle.bundle import Bundle
+# Matching properties dictionary
+match_on = {'FileObjectType': ['file_name'],
+            'WindowsRegistryKeyObjectType': ['key'],
+            'WindowsMutexObjectType': ['name']}
 
 bundle1 = Bundle.from_obj(maec_bundle_binding.parse("zeus_anubis_maec.xml"))
 bundle2 = Bundle.from_obj(maec_bundle_binding.parse("zeus_threatexpert_maec.xml"))
 
-comparator = Bundle.compare([bundle1, bundle2])
+comparator = Bundle.compare([bundle1, bundle2], match_on)
+
 pprint.pprint(comparator.get_common())
 pprint.pprint(comparator.get_unique())
 
