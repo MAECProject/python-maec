616df0ca5488969d90e31c2e8cd1882a0a0c3a49
Ivan Kirillov <ikirillov@mitre.org>
2013-10-11 15:14:27 -0400

    Updated to reflect case sensitive option

--- examples/comparator_example.py
+++ examples/comparator_example.py
@@ -2,14 +2,14 @@ import pprint
 import maec.bindings.maec_bundle as maec_bundle_binding
 from maec.bundle.bundle import Bundle
 # Matching properties dictionary
-match_on = {'FileObjectType': ['file_name'],
-            'WindowsRegistryKeyObjectType': ['hive', 'values.name/data'],
-            'WindowsMutexObjectType': ['name']}
+match_on_dictionary = {'FileObjectType': ['file_name'],
+                       'WindowsRegistryKeyObjectType': ['hive', 'values.name/data'],
+                       'WindowsMutexObjectType': ['name']}
 # Parse in the input Bundle documents and create their python-maec Bundle class representations
 bundle1 = Bundle.from_obj(maec_bundle_binding.parse("zeus_anubis_maec.xml"))
 bundle2 = Bundle.from_obj(maec_bundle_binding.parse("zeus_threatexpert_maec.xml"))
 # Perform the comparison and get the results
-comparison_results = Bundle.compare([bundle1, bundle2], match_on)
+comparison_results = Bundle.compare([bundle1, bundle2], match_on = match_on_dictionary, case_sensitive = False)
 # Pretty print the common and unique Objects
 print "******Common Objects:*******\n"
 pprint.pprint(comparison_results.get_common())
