cad1062af54cb053fd707623c2a453860b0b6ca5
Ivan Kirillov <ikirillov@mitre.org>
2013-10-11 13:54:03 -0400

    Updated comaparator test 2 with more comments

--- examples/comparator_test_2.py
+++ examples/comparator_test_2.py
@@ -3,17 +3,17 @@ import maec.bindings.maec_bundle as maec_bundle_binding
 from maec.bundle.bundle import Bundle
 # Matching properties dictionary
 match_on = {'FileObjectType': ['file_name'],
-            'WindowsRegistryKeyObjectType': ['key'],
+            'WindowsRegistryKeyObjectType': ['hive', 'values.name/data'],
             'WindowsMutexObjectType': ['name']}
-
+# Parse in the input Bundle documents and create their python-maec Bundle class representations
 bundle1 = Bundle.from_obj(maec_bundle_binding.parse("zeus_anubis_maec.xml"))
 bundle2 = Bundle.from_obj(maec_bundle_binding.parse("zeus_threatexpert_maec.xml"))
-
-comparator = Bundle.compare([bundle1, bundle2], match_on)
-
+# Perform the comparison and get the results
+comparison_results = Bundle.compare([bundle1, bundle2], match_on)
+# Pretty print the common and unique Objects
 print "******Common Objects:*******\n"
-pprint.pprint(comparator.get_common())
+pprint.pprint(comparison_results.get_common())
 print "****************************"
 print "******Unique Objects:*******\n"
-pprint.pprint(comparator.get_unique())
+pprint.pprint(comparison_results.get_unique())
 print "****************************"
