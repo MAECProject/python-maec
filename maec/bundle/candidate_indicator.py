262aa3edf9302629a08d99bcbe7737ce36e84f28
Ivan Kirillov <ikirillov@mitre.org>
2013-10-22 08:50:49 -0400

    Fixed typo in CandidateIndicator constructor

--- maec/bundle/candidate_indicator.py
+++ maec/bundle/candidate_indicator.py
@@ -14,7 +14,7 @@ from cybox.common import VocabString
 from cybox.core import ActionReference
 
 class CandidateIndicator(maec.Entity):
-    def init(self, id, generator):
+    def __init__(self, id, generator):
         super(CandidateIndicator, self).__init__()
         if id is not None:
             self.id = id
