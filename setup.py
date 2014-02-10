13ff276ba71a9f7e17ffc546ed428c73c0d205aa
Ivan Kirillov <ikirillov@mitre.org>
2014-02-10 10:08:48 -0500

    Updated python-cybox version range for compatibility

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     description="An API for parsing and creating MAEC content.",
     url="http://maec.mitre.org",
     packages=find_packages(),
-    install_requires=['lxml>=2.3','cybox>=2.0.1.0,<2.0.2.0']
+    install_requires=['lxml>=2.3','cybox>=2.1.0.0,<2.2.0.0']
 )
