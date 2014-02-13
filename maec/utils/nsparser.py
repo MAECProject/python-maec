bd2c6c8d13e1832ceb483ed5479466b1bbcb9955
Ivan Kirillov <ikirillov@mitre.org>
2014-02-13 13:36:01 -0500

    Added Metadata class and META instance, based on CybOX

--- maec/utils/nsparser.py
+++ maec/utils/nsparser.py
@@ -8,9 +8,42 @@
 
 import maec.bindings.maec_bundle as bundle_binding
 import maec.bindings.maec_package as package_binding
-from cybox.utils import NamespaceParser, META
+from cybox.utils import NamespaceParser, META, Namespace
 
 import itertools
+
+class Metadata(object):
+    """Metadata about MAEC namespaces."""
+
+    def __init__(self, namespace_list):
+        self._ns_dict = {}
+        self._prefix_dict = {}
+
+        for ns in namespace_list:
+            n = Namespace(*ns)
+            self.add_namespace(n)
+
+    def add_namespace(self, namespace):
+        self._ns_dict[namespace.name] = namespace
+        self._prefix_dict[namespace.prefix] = namespace
+
+    def lookup_namespace(self, namespace):
+        return self._ns_dict.get(namespace)
+
+    def lookup_prefix(self, prefix):
+        return self._prefix_dict.get(prefix)
+
+
+# A list of (namespace, prefix, schemalocation) tuples
+# This is loaded by the Metadata class and should not be accessed directly.
+NS_LIST = [
+    ('http://www.w3.org/2001/XMLSchema-instance', 'xsi', ''),
+    ('http://maec.mitre.org/XMLSchema/maec-bundle-4', 'maecBundle', 'http://maec.mitre.org/language/version4.1/maec_bundle_schema.xsd'),
+    ('http://maec.mitre.org/XMLSchema/maec-package-2', 'maecPackage', 'http://maec.mitre.org/language/version4.1/maec_package_schema.xsd'),
+    ('http://maec.mitre.org/default_vocabularies-1', 'maecVocabs', 'http://maec.mitre.org/language/version4.1/maec_default_vocabularies.xsd'),
+]
+
+META = Metadata(NS_LIST)
 
 class MAECNamespaceParser(NamespaceParser):
     superclass = NamespaceParser
