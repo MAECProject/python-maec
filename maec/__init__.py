27f3ec3d3532b2bfba524fe50ec4f48e2ff5524a
Ivan Kirillov <ikirillov@mitre.org>
2014-09-18 09:02:39 -0400

    Updated maec.Entity and maec.EntityList to more naturally derive from python-cybox

--- maec/__init__.py
+++ maec/__init__.py
@@ -8,7 +8,7 @@ from StringIO import StringIO
 import bindings.maec_bundle as bundle_binding
 import bindings.maec_package as package_binding
 from cybox import Entity as cyboxEntity
-from cybox import EntityList as cyboxEntityList
+from cybox import EntityList
 from cybox import TypedField
 from cybox.utils import Namespace
 from maec.utils import maecMETA, EntityParser
@@ -47,7 +47,7 @@ class Entity(cyboxEntity):
             namespace_dict = self.__input_namespaces__
         out_file  = open(filename, 'w')
         out_file.write("<?xml version='1.0' encoding='UTF-8'?>\n")
-        self.to_obj().export(out_file, 0, namespacedef_ = self._get_namespace_def(namespace_dict))
+        self.to_obj().export(out_file.write, 0, namespacedef_ = self._get_namespace_def(namespace_dict))
         out_file.close()
 
     def _get_namespace_def(self, additional_ns_dict=None):
@@ -106,152 +106,6 @@ class Entity(cyboxEntity):
                     if isinstance(item, Entity) or isinstance(item, cyboxEntity):
                         yield item
 
-    @classmethod
-    def from_dict(cls, cls_dict=None):
-        if cls_dict is None:
-            return None
-
-        entity = cls()
-
-        # Shortcut if an actual dict is not provided:
-        if not isinstance(cls_dict, dict):
-            value = cls_dict
-            # Call the class's constructor
-            try:
-                return cls(value)
-            except TypeError:
-                raise TypeError("Could not instantiate a %s from a %s: %s" %
-                                (cls, type(value), value))
-
-        for field in cls._get_vars():
-            val = cls_dict.get(field.key_name)
-            if field.type_:
-                if issubclass(field.type_, EntityList) or issubclass(field.type_, cyboxEntityList):
-                    val = field.type_.from_list(val)
-                elif field.multiple:
-                    if val is not None:
-                        val = [field.type_.from_dict(x) for x in val]
-                    else:
-                        val = []
-                else:
-                    val = field.type_.from_dict(val)
-            else:
-                if field.multiple and not val:
-                    val = []
-            setattr(entity, field.attr_name, val)
-
-        return entity
-
-class EntityList(collections.MutableSequence, Entity):
-    _contained_type = object
-
-    # Don't try to cast list types (yet)
-    _try_cast = False
-
-    def __init__(self, *args):
-        super(EntityList, self).__init__()
-        self._inner = []
-
-        for arg in args:
-            if isinstance(arg, list):
-                self.extend(arg)
-            else:
-                self.append(arg)
-
-    def __getitem__(self, key):
-        return self._inner.__getitem__(key)
-
-    def __setitem__(self, key, value):
-        if not self._is_valid(value):
-            value = self._fix_value(value)
-        self._inner.__setitem__(key, value)
-
-    def __delitem__(self, key):
-        self._inner.__delitem__(key)
-
-    def __len__(self):
-        return len(self._inner)
-
-    def insert(self, idx, value):
-        if not self._is_valid(value):
-            value = self._fix_value(value)
-        self._inner.insert(idx, value)
-
-    def _is_valid(self, value):
-        """Check if this is a valid object to add to the list.
-
-        Subclasses can override this function, but it's probably better to
-        modify the istypeof function on the _contained_type.
-        """
-        return self._contained_type.istypeof(value)
-
-    def _fix_value(self, value):
-        """Attempt to coerce value into the correct type.
-
-        Subclasses can override this function.
-        """
-        try:
-            new_value = self._contained_type(value)
-        except:
-            raise ValueError("Can't put '%s' (%s) into a %s" %
-                (value, type(value), self.__class__))
-        return new_value
-
-    # The next four functions can be overridden, but otherwise define the
-    # default behavior for EntityList subclasses which define the following
-    # class-level members:
-    # - _binding_class
-    # - _binding_var
-    # - _contained_type
-
-    def to_obj(self):
-        tmp_list = [x.to_obj() for x in self]
-
-        list_obj = self._binding_class()
-
-        setattr(list_obj, self._binding_var, tmp_list)
-
-        return list_obj
-
-    def to_list(self):
-        return [h.to_dict() for h in self]
-
-    # Alias the `to_list` function as `to_dict`
-    to_dict = to_list
-
-    @classmethod
-    def from_obj(cls, list_obj):
-        if not list_obj:
-            return None
-
-        list_ = cls()
-
-        for item in getattr(list_obj, cls._binding_var):
-            list_.append(cls._contained_type.from_obj(item))
-
-        return list_
-
-    @classmethod
-    def from_list(cls, list_list):
-        if not isinstance(list_list, list):
-            return None
-
-        list_ = cls()
-
-        for item in list_list:
-            list_.append(cls._contained_type.from_dict(item))
-
-        return list_
-
-    @classmethod
-    def object_from_list(cls, entitylist_list):
-        """Convert from list representation to object representation."""
-        return cls.from_list(entitylist_list).to_obj()
-
-    @classmethod
-    def list_from_object(cls, entitylist_obj):
-        """Convert from object representation to list representation."""
-        return cls.from_obj(entitylist_obj).to_list()
 
 def parse_xml_instance(filename, check_version = True):
     """Parse a MAEC instance and return the correct Binding and API objects
