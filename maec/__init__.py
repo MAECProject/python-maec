ececba669824351a75196cf8dd2a215a829347c0
Ivan Kirillov <ikirillov@mitre.org>
2014-11-12 10:26:26 -0500

    Updated custom_header in to_xml_file to encompass script_options

--- maec/__init__.py
+++ maec/__init__.py
@@ -38,7 +38,7 @@ def get_schemaloc_string(ns_set):
 class Entity(cyboxEntity):
     """Base class for all classes in the MAEC SimpleAPI."""
 
-    def to_xml_file(self, filename, namespace_dict=None, custom_header=None, options_used=None):
+    def to_xml_file(self, filename, namespace_dict=None, custom_header=None):
         """Export an object to an XML file. Only supports Package or Bundle objects at the moment."""
         # Update the namespace dictionary with namespaces found upon import
         if namespace_dict and hasattr(self, '__input_namespaces__'):
@@ -46,19 +46,20 @@ class Entity(cyboxEntity):
         elif not namespace_dict and hasattr(self, '__input_namespaces__'):
             namespace_dict = self.__input_namespaces__
         out_file  = open(filename, 'w')
-        if custom_header:
+        out_file.write("<?xml version='1.0' encoding='UTF-8'?>\n")
+        # Write out the custom header, if included
+        if isinstance(custom_header, list):
             for line in custom_header:
                 out_file.write[line]
-        else:
-            out_file.write("<?xml version='1.0' encoding='UTF-8'?>\n")
-            
-        if options_used:
+        elif isinstance(custom_header, dict):
             out_file.write("<!--\n")
-            for key, value in options_used.iteritems():
+            for key, value in custom_header.iteritems():
                 sanitized_key = str(key).replace("-->", "\\-\\->")
                 sanitized_value = str(value).replace("-->", "\\-\\->")
                 out_file.write(sanitized_key + ": " + sanitized_value + "\n")
             out_file.write("-->\n")
+        elif isinstance(custom_header, basestring):
+            out_file.write(custom_header)
             
         self.to_obj().export(out_file.write, 0, namespacedef_ = self._get_namespace_def(namespace_dict))
         out_file.close()
