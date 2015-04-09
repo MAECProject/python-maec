58abf1c91b32306cf56ed68bd4439d239e2a3374
Bryan Worrell <bworrell@mitre.org>
2015-04-09 14:34:17 -0400

    Added flip_dict utility method.

--- maec/utils/__init__.py
+++ maec/utils/__init__.py
@@ -1,13 +1,23 @@
-#MAEC Utility Methods
-
-#Copyright (c) 2015, The MITRE Corporation
-#All rights reserved
-
-
-#Compatible with MAEC v4.1
-#Last updated 02/18/2014
-
+# Copyright (c) 2015, The MITRE Corporation
+# All rights reserved
 """MAEC utility methods"""
+
+
+def flip_dict(d):
+    """Returns a copy of the input dictionary `d` where the values of `d`
+    become the keys and the keys become the values.
+
+    Note:
+        This does not even attempt to address key collisions.
+
+    Args:
+        d: A dictionary
+
+    """
+    return dict((v,k) for k, v in d.iteritems())
+
+
+# Namespace flattening
 import maec
 from .nsparser import maecMETA
 from .idgen import *
@@ -15,8 +25,3 @@ from .parser import EntityParser
 from .comparator import (ObjectHash, BundleComparator, SimilarObjectCluster,
                          ComparisonResult)
 from .deduplicator import BundleDeduplicator
-
-
-
-    
-
