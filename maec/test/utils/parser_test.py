8b0ba66380194d7d29cb65cd77887923e757eb80
Greg Back <gback@mitre.org>
2015-07-15 14:19:16 -0500

    Use mixbox parser.

---
+++ maec/test/utils/parser_test.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2015, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+from StringIO import StringIO
+import unittest
+
+from mixbox.parser import (UnknownVersionError, UnsupportedRootElementError,
+                           UnsupportedVersionError)
+
+from maec.bundle import Bundle
+from maec.package import Package
+from maec.utils import EntityParser
+
+
+class ParserTests(unittest.TestCase):
+
+    def test_valid_package(self):
+        valid_package = """
+        <maecPackage:MAEC_Package
+            xmlns:maecPackage="http://maec.mitre.org/XMLSchema/maec-package-2"
+            id="example:package-1" schema_version="2.1">
+        </maecPackage:MAEC_Package>
+        """
+
+        parser = EntityParser()
+        package = parser.parse_xml(StringIO(valid_package))
+
+        self.assertEqual(Package, type(package))
+        self.assertEqual("example:package-1", package.id_)
+
+    def test_valid_bundle(self):
+        valid_bundle = """
+        <maecBundle:MAEC_Bundle
+            xmlns:maecBundle="http://maec.mitre.org/XMLSchema/maec-bundle-4"
+            id="example:bundle-1" schema_version="4.1">
+        </maecBundle:MAEC_Bundle>
+        """
+
+        parser = EntityParser()
+        package = parser.parse_xml(StringIO(valid_bundle))
+
+        self.assertEqual("example:bundle-1", package.id_)
+
+    def test_wrong_root_element(self):
+        wrong_root = """
+        <maecPackage:NotAPackage
+            xmlns:maecPackage="http://maec.mitre.org/XMLSchema/maec-package-2"
+            id="example:package-1" schema_version="2.1">
+        </maecPackage:NotAPackage>
+        """
+
+        parser = EntityParser()
+        self.assertRaises(UnsupportedRootElementError,
+                          parser.parse_xml, StringIO(wrong_root))
+
+        # If there's not a valid root element, there's no way to check the
+        # version number.
+        self.assertRaises(UnsupportedVersionError,
+                          parser.parse_xml, StringIO(wrong_root),
+                          check_root=False)
+
+    def test_wrong_version(self):
+        wrong_version = """
+        <maecPackage:MAEC_Package
+            xmlns:maecPackage="http://maec.mitre.org/XMLSchema/maec-package-2"
+            id="example:package-1" schema_version="10.1.8">
+        </maecPackage:MAEC_Package>
+        """
+
+        parser = EntityParser()
+        self.assertRaises(UnsupportedVersionError,
+                          parser.parse_xml, StringIO(wrong_version))
+
+        package = parser.parse_xml(StringIO(wrong_version),
+                                   check_version=False)
+
+        self.assertEqual("example:package-1", package.id_)
+        self.assertEqual("10.1.8", package.schema_version)
+
+    def test_missing_version(self):
+        missing_version = """
+        <maecPackage:MAEC_Package
+            xmlns:maecPackage="http://maec.mitre.org/XMLSchema/maec-package-2"
+            id="example:package-1">
+        </maecPackage:MAEC_Package>
+        """
+
+        parser = EntityParser()
+        self.assertRaises(UnknownVersionError,
+                          parser.parse_xml, StringIO(missing_version))
+
+        package = parser.parse_xml(StringIO(missing_version),
+                                   check_version=False)
+
+        self.assertEqual("example:package-1", package.id_)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- maec/utils/parser.py
+++ maec/utils/parser.py
@@ -1,114 +1,37 @@
 # Copyright (c) 2015, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-import maec
-from lxml import etree
+import mixbox.parser
+from mixbox.parser import (UnknownVersionError, UnsupportedVersionError,
+                           UnsupportedRootElementError)
 
-class UnsupportedVersionError(Exception):
-    pass
+# Alias for backwards compatibility
+UnsupportedRootElement = UnsupportedRootElementError
 
-class UnknownVersionError(Exception):
-    pass
+TAG_MAEC_BUNDLE = "{http://maec.mitre.org/XMLSchema/maec-bundle-4}MAEC_Bundle"
+TAG_MAEC_PACKAGE = "{http://maec.mitre.org/XMLSchema/maec-package-2}MAEC_Package"
 
-class UnsupportedRootElement(Exception):
-    pass
 
-class EntityParser(object):
-    def __init__(self):
-        self.is_bundle = False
-        self.is_package = False
+class EntityParser(mixbox.parser.EntityParser):
 
-    def _check_version(self, tree):
-        '''Returns true of the instance document @tree is a version supported by python-maec'''
+    def supported_tags(self):
+        return [TAG_MAEC_BUNDLE, TAG_MAEC_PACKAGE]
 
-        try:
-            root = tree.getroot() # is tree an lxml.Element or lxml.ElementTree
-        except AttributeError:
-            root = tree
+    def get_version(self, root):
+        return root.attrib.get('schema_version')
 
-        if not root.attrib.get('schema_version'):
-            raise UnknownVersionError("No version attribute set on xml instance. Unable to determine version compatibility")
+    def supported_versions(self, tag):
+        if tag == TAG_MAEC_BUNDLE:
+            return ['4.1']
+        elif tag == TAG_MAEC_PACKAGE:
+            return ['2.1']
+        else:
+            return []
 
-        python_maec_version = maec.__version__ # ex: '4.1.0.0'
-        supported_maec_version = ('4.1', '2.1') # ex: '4.1.0'
-        document_version = root.attrib['schema_version']
-
-        if document_version not in supported_maec_version:
-            raise UnsupportedVersionError("Your python-maec library supports MAEC %s, or the MAEC Bundle Schema at %s and MAEC Package Schema at %s. Document version was %s" % (supported_maec_version[0], supported_maec_version[0], supported_maec_version[1], document_version))
-
-        return True
-
-    def _check_root(self, tree):
-        try:
-            root = tree.getroot() # is tree an lxml.Element or lxml.ElementTree
-        except AttributeError:
-            root = tree
-        # General compatibility check
-        if root.tag not in ("{http://maec.mitre.org/XMLSchema/maec-bundle-4}MAEC_Bundle", "{http://maec.mitre.org/XMLSchema/maec-package-2}MAEC_Package"):
-            raise UnsupportedRootElement("Document root element must be an instance of MAEC_Package or MAEC_Bundle")
-
-        # Determine if we're dealing with a MAEC Bundle or MAEC Package
-        if "MAEC_Bundle" in root.tag:
-            self.is_bundle = True
-        elif "MAEC_Package" in root.tag:
-            self.is_package = True
-
-        return True
-
-    def _apply_input_namespaces(self, tree, entity):
-        try:
-            root = tree.getroot() # is tree an lxml.Element or lxml.ElementTree
-        except AttributeError:
-            root = tree
-        
-        entity.__input_namespaces__ = dict(root.nsmap.iteritems())
-
-    def parse_xml_to_obj(self, xml_file, check_version=True):
-        """Creates a MAEC binding object from the supplied xml file.
-
-        Arguments:
-        xml_file -- A filename/path or a file-like object reprenting a MAEC instance document
-        check_version -- Inspect the version before parsing.
-        """
-        parser = etree.ETCompatXMLParser(huge_tree=True, resolve_entities=False)
-        tree = etree.parse(xml_file, parser=parser)
-
-        # Check the root and determine the type of document we're dealing with
-        self._check_root(tree)
-
-        if check_version:
-            self._check_version(tree)
-
-        binding_obj = None
-        if self.is_package:
-            import maec.bindings.maec_package as maec_package_binding
-            binding_obj = maec_package_binding.PackageType().factory()
-            binding_obj.build(tree.getroot())
-        elif self.is_bundle:
-            import maec.bindings.maec_bundle as maec_bundle_binding
-            binding_obj = maec_bundle_binding.BundleType().factory()
-            binding_obj.build(tree.getroot())
-
-        return binding_obj
-
-    def parse_xml(self, xml_file, check_version=True):
-        """Creates a python-maec Bundle or Package object from the supplied xml_file.
-
-        Arguments:
-        xml_file -- A filename/path or a file-like object reprenting a MAEC instance (i.e. Package or Bundle) document
-        check_version -- Inspect the version before parsing.
-        """
-        parser = etree.ETCompatXMLParser(huge_tree=True, resolve_entities=False)
-        tree = etree.parse(xml_file, parser=parser)
-
-        api_obj = None
-        binding_obj = self.parse_xml_to_obj(xml_file, check_version)
-        if self.is_package:
-            from maec.package.package import Package # resolve circular dependencies
-            api_obj = Package.from_obj(binding_obj)
-        elif self.is_bundle:
-            from maec.bundle.bundle import Bundle # resolve circular dependencies
-            api_obj = Bundle.from_obj(binding_obj)
-        self._apply_input_namespaces(tree, api_obj)
-
-        return api_obj
+    def get_entity_class(self, tag):
+        if tag == TAG_MAEC_BUNDLE:
+            from maec.bundle import Bundle
+            return Bundle
+        elif tag == TAG_MAEC_PACKAGE:
+            from maec.package import Package
+            return Package
